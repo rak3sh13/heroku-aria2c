e3999b859c8da3cfd986d3aabab0a732c42365ac
fanweiya <fanweiya@users.noreply.github.com>
2020-06-10 21:40:25 +0800

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,29 +1,107 @@
 #!/bin/bash
 
-filePath=$3
-relativePath=${filepath#./downloads/}
-topPath=./downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
-
+# 限制最低上传大小，仅 BT 多文件下载时有效，用于过滤无用文件。低于此大小的文件将被删除，不会上传。
+MIN_SIZE=10m
+# 排除文件类型，仅 BT 多文件下载时有效，用于过滤无用文件。排除的文件将被删除，不会上传。
+EXCLUDE_FILE='html,url,lnk,txt,jpg,png'
+#============================================================
+filePath=$3                                   # Aria2传递给脚本的文件路径。BT下载有多个文件时该值为文件夹内第一个文件，如/root/Download/a/b/1.mp4
+RELATIVE_PATH=${filePath#./downloads/}   # 路径转换，去掉开头的下载路径。
+TOP_PATH=./downloads/${RELATIVE_PATH%%/*} # 路径转换，BT下载文件夹时为顶层文件夹路径，普通单文件下载时与文件路径相同。
+RED_FONT_PREFIX="\033[31m"
 LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
+YELLOW_FONT_PREFIX="\033[1;33m"
+LIGHT_PURPLE_FONT_PREFIX="\033[1;35m"
 FONT_COLOR_SUFFIX="\033[0m"
 INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
+ERROR="[${RED_FONT_PREFIX}ERROR${FONT_COLOR_SUFFIX}]"
+WARRING="[${YELLOW_FONT_PREFIX}WARRING${FONT_COLOR_SUFFIX}]"
 
-echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file ..."
+TASK_INFO() {
+    echo -e "
+-------------------------- [${YELLOW_FONT_PREFIX}TASK INFO${FONT_COLOR_SUFFIX}] --------------------------
+${LIGHT_PURPLE_FONT_PREFIX}Download path:${FONT_COLOR_SUFFIX} ./downloads
+${LIGHT_PURPLE_FONT_PREFIX}File path:${FONT_COLOR_SUFFIX} ${filePath}
+${LIGHT_PURPLE_FONT_PREFIX}Upload path:${FONT_COLOR_SUFFIX} ${UPLOAD_PATH}
+${LIGHT_PURPLE_FONT_PREFIX}Remote path:${FONT_COLOR_SUFFIX} ${REMOTE_PATH}
+-------------------------- [${YELLOW_FONT_PREFIX}TASK INFO${FONT_COLOR_SUFFIX}] --------------------------
+"
+}
 
-if [ $2 -eq 0 ]; then
+CLEAN_UP() {
+    [[ -n ${MIN_SIZE} || -n ${INCLUDE_FILE} || -n ${EXCLUDE_FILE} ]] && echo -e "${INFO} Clean up excluded files ..."
+    [[ -n ${MIN_SIZE} ]] && rclone delete -v --config="rclone.conf" "${UPLOAD_PATH}" --max-size ${MIN_SIZE}
+    [[ -n ${INCLUDE_FILE} ]] && rclone delete -v --config="rclone.conf" "${UPLOAD_PATH}" --exclude "*.{${INCLUDE_FILE}}"
+    [[ -n ${EXCLUDE_FILE} ]] && rclone delete -v --config="rclone.conf" "${UPLOAD_PATH}" --include "*.{${EXCLUDE_FILE}}"
+}
+
+UPLOAD_FILE() {
+    RETRY=0
+    while [ ${RETRY} -le ${RETRY_NUM} ]; do
+        [ ${RETRY} != 0 ] && (
+            echo
+            echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
+            echo
+        )
+        rclone move -v --config="rclone.conf" "${UPLOAD_PATH}" "${REMOTE_PATH}"
+        RCLONE_EXIT_CODE=$?
+        if [ ${RCLONE_EXIT_CODE} -eq 0 ]; then
+            [ -e "${DOT_ARIA2_FILE}" ] && rm -vf "${DOT_ARIA2_FILE}"
+            rclone rmdirs -v --config="rclone.conf" "./downloads" --leave-root
+            echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Upload done: ${UPLOAD_PATH} -> ${REMOTE_PATH}"
+            [ $LOG_PATH ] && echo -e "$(date +"%m/%d %H:%M:%S") [INFO] Upload done: ${UPLOAD_PATH} -> ${REMOTE_PATH}" >>${LOG_PATH}
+            break
+        else
+            RETRY=$((${RETRY} + 1))
+            [ ${RETRY} -gt ${RETRY_NUM} ] && (
+                echo
+                echo -e "$(date +"%m/%d %H:%M:%S") ${ERROR} Upload failed: ${UPLOAD_PATH}"
+                [ $LOG_PATH ] && echo -e "$(date +"%m/%d %H:%M:%S") [ERROR] Upload failed: ${UPLOAD_PATH}" >>${LOG_PATH}
+                echo
+            )
+            sleep 3
+        fi
+    done
+}
+
+UPLOAD() {
+    echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Start upload..."
+    TASK_INFO
+    UPLOAD_FILE
+}
+
+if [ -z $2 ]; then
+    echo && echo -e "${ERROR} This script can only be used by passing parameters through Aria2."
+    echo && echo -e "${WARRING} 直接运行此脚本可能导致无法开机！"
+    exit 1
+elif [ $2 -eq 0 ]; then
     exit 0
-elif [ -e "${filepath}.aria2" ]; then
-    rm -vf "${filepath}.aria2"
-elif [ -e "${topPath}.aria2" ]; then
-    rm -vf "${topPath}.aria2"
-fi
-echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
-echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
-
-if [[ $2 -eq 1 ]]; then # single file
-	rclone -v --config="rclone.conf" --no-traverse copy "$3" "$RCLONE_DESTINATION" 2>&1	
-elif [[ $2 -gt 1 ]]; then # multiple file
-	rclone -v --config="rclone.conf" --no-traverse copy "$topPath" "$RCLONE_DESTINATION/${relativePath%%/*}"
 fi
 
-echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
+
+if [ -e "${filePath}.aria2" ]; then
+    DOT_ARIA2_FILE="${filePath}.aria2"
+elif [ -e "${TOP_PATH}.aria2" ]; then
+    DOT_ARIA2_FILE="${TOP_PATH}.aria2"
+fi
+
+if [ "${TOP_PATH}" = "${filePath}" ] && [ $2 -eq 1 ]; then # 普通单文件下载，移动文件到设定的网盘文件夹。
+    UPLOAD_PATH="${filePath}"
+    REMOTE_PATH="$RCLONE_DESTINATION"
+    UPLOAD
+    exit 0
+elif [ "${TOP_PATH}" != "${filePath}" ] && [ $2 -gt 1 ]; then # BT下载（文件夹内文件数大于1），移动整个文件夹到设定的网盘文件夹。
+    UPLOAD_PATH="${TOP_PATH}"
+    REMOTE_PATH="$RCLONE_DESTINATION/${RELATIVE_PATH%%/*}"
+    CLEAN_UP
+    UPLOAD
+    exit 0
+elif [ "${TOP_PATH}" != "${filePath}" ] && [ $2 -eq 1 ]; then # 第三方度盘工具下载（子文件夹或多级目录等情况下的单文件下载）、BT下载（文件夹内文件数等于1），移动文件到设定的网盘文件夹下的相同路径文件夹。
+    UPLOAD_PATH="${filePath}"
+    REMOTE_PATH="$RCLONE_DESTINATION/${RELATIVE_PATH%/*}"
+    UPLOAD
+    exit 0
+fi
+
+echo -e "${ERROR} Unknown error."
+TASK_INFO
